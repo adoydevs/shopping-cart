4fa272f765cd38b0eba5403dae8f41cd8a306572
adoydevs <imranadoy@gmail.com>
2023-08-11 01:56:24 +0600

    add

--- js/script.js
+++ js/script.js
@@ -13,10 +13,14 @@ document.getElementById('case-plus-btn').addEventListener('click' , function(){
     const getProductPriceString = getProductPrice.innerText;
     const previousProductPrice = parseInt(getProductPriceString);
 
-    const newProductPrice = previousProductPrice + 120;
+    const newProductPrice = getNewNumber * 120;
 
 
     getProductPrice.innerText = newProductPrice;
+
+
+
+
 
 } )
 
@@ -34,57 +38,67 @@ document.getElementById('case-minus-btn').addEventListener('click' , function(){
     const decreaseProductPriceString = decreaseProductPrice.innerText;
     const getDecreaseProductPrice = parseInt(decreaseProductPriceString);
 
-    const newDecreaseProductPrice = getDecreaseProductPrice - 120;
+
+    const newProductPrice = getMinusNewNumber * 120;
 
 
-    decreaseProductPrice.innerText = newDecreaseProductPrice;
+    decreaseProductPrice.innerText = newProductPrice;
+
+   
 
 
 } )
+
+
+
+// ........................AbortController...........
+
+
 
 document.getElementById('case-plus-button').addEventListener('click' , function(){
     const currentPlusNumber = document.getElementById('get-product-num');
     const currentPlusNumberString = currentPlusNumber.value;
     const getCurrentPlusNumber = parseInt(currentPlusNumberString);
 
-    currentPlusNumber.value = getCurrentPlusNumber + 1;
+    const getFinalPlusNumber = getCurrentPlusNumber + 1;
+
+    currentPlusNumber.value = getFinalPlusNumber;
+
+
 
     const currentProductPrice = document.getElementById('case-price');
     const currentProductPriceString = currentProductPrice.innerText;
     const getCurrentProductPrice = parseInt(currentProductPriceString);
 
-    const getNewProductPrice = getCurrentProductPrice + 59;
+    const getNewProductPrice = getFinalPlusNumber * 59;
     currentProductPrice.innerText = getNewProductPrice;
 
-
-
 })
+
 
 document.getElementById('case-minus-button').addEventListener('click' , function(){
     const currentMinusNumber = document.getElementById('get-product-num');
     const currentMinusNumberString = currentMinusNumber.value;
     const getCurrentMinusNumber = parseInt(currentMinusNumberString);
 
-    currentMinusNumber.value = getCurrentMinusNumber - 1;
+    const getFinalMinusNumber = getCurrentMinusNumber - 1;
+    currentMinusNumber.value = getFinalMinusNumber;
 
-    document.getElementById('case-price');
-
-
+    
     const currentProductPrice = document.getElementById('case-price');
     const currentProductPriceString = currentProductPrice.innerText;
     const getCurrentProductPrice = parseInt(currentProductPriceString);
 
-    const getNewProductPrice = getCurrentProductPrice - 59;
+
+    const getNewProductPrice = getFinalMinusNumber * 59;
+
     currentProductPrice.innerText = getNewProductPrice;
+
 
 })
 
-// document.getElementById()
 
 
-// const subtotalPrice = document.getElementById('subtotal-price');
-//     const subtotalPriceString = subtotalPrice.innerText;
-//     const getSubTotalPrice = parseInt(subtotalPriceString);
 
-//     const getSubtotal = newProductPrice + getNewProductPrice;
-//     subtotalPrice.innerText = getSubtotal;
+
+
